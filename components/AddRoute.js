84657b5ae1b9d5f20e19692afb9ce3a5198c5d5a
Alex Price <alex@alexprice.co>
2016-12-27 16:57:45 -0600

    Successfully animated indicator 1
    Porting over to the others…

--- components/AddRoute.js
+++ components/AddRoute.js
@@ -24,93 +24,79 @@ export default class AddRoute extends Component {
             inputsFrom: '',
             inputsName: '',
             inputs: {
-              to: '',
-              from: '',
-              when: new Date(new Date().setMinutes(0)),
-              name: ''
+                to: '',
+                from: '',
+                when: new Date(new Date().setMinutes(0)),
+                name: ''
             },
             currentStep: 1,
-            indicator1: {
-                width: 8,
-                height: 8,
-                margin: 6,
-                borderRadius: 100,
-                backgroundColor: '#FFFFFF',
-                borderColor: '#2E80ED',
-                borderWidth: 2
-            },
             indicator2: {
-                width: 8,
-                height: 8,
-                margin: 6,
-                borderRadius: 100,
-                backgroundColor: '#CCCCCC'
+                status: new Animated.Value(0),
+                backgroundColor: 'rgb(204,204,204)',
+                borderColor: 'rgb(204,204,204)',
             },
             indicator3: {
-                width: 8,
-                height: 8,
-                margin: 6,
-                borderRadius: 100,
-                backgroundColor: '#CCCCCC'
+                status: new Animated.Value(0),
+                backgroundColor: 'rgb(204,204,204)',
+                borderColor: 'rgb(204,204,204)',
             },
             indicator4: {
-                width: 8,
-                height: 8,
-                margin: 6,
-                borderRadius: 100,
-                backgroundColor: '#CCCCCC'
+                status: new Animated.Value(0),
+                backgroundColor: 'rgb(204,204,204)',
+                borderColor: 'rgb(204,204,204)',
             },
-        }
+            indicator1: {}
+        },
+        this.state.indicator1.status = new Animated.Value(50),
+        this.state.indicator1.backgroundColor = this.state.indicator1.status.interpolate({
+            inputRange: [0, 50, 100],
+            outputRange: ['rgb(255,255,255)', 'rgb(255,255,255)', 'rgb(46,128,237)']
+        }),
+        this.state.indicator1.borderColor = 'rgb(46,128,237)'
     }
 
     // Handles animation and updates for the dot indicators
     _setIndicatorState() {
-        let indicator =  {
-            width: 8,
-            height: 8,
-            margin: 6,
-            borderRadius: 100,
-            borderWidth: 2,
 
-            backgroundColor: '#CCCCCC',
-            borderColor: '#CCCCCC',
-        }
+        Animated.timing(this.state.indicator1.status, {
+            toValue: 100,
+            duration: 300
+        }).start(() => {
+            console.log('Done animating: ', this.state.indicator1.status)
+        })
 
-        let indicatorActive = {
-            width: 8,
-            height: 8,
-            margin: 6,
-            borderRadius: 100,
-            borderWidth: 2,
 
-            backgroundColor: '#FFFFFF',
-            borderColor: '#2E80ED',
-        }
 
-        let indicatorFilled = {
-            width: 8,
-            height: 8,
-            margin: 6,
-            borderRadius: 100,
-            borderWidth: 2,
-
-            backgroundColor: '#2E80ED',
-            borderColor: '#2E80ED',
-        }
+        // this.colorValue = new Animated.Value(0)
+        //
+        // colorChange() {
+        //     this.colorValue.setValue(0)
+        //     Animated.timing(this.colorValue, {
+        //         toValue: 100,
+        //         duration: 5000
+        //     }).start(() => {
+        //         this.colorChange()
+        //     })
+        // }
+        //
+        // const colorAnimation = this.colorValue.interpolate({
+        //     inputRange: [0, 50, 100],
+        //     outputRange: ['yellow', 'orange', 'red']
+        // })
 
         // The best way we could find to dynamically update the indicator styles
         // based on the currentStep value...
-        var keyCurrent = 'indicator' + (this.state.currentStep)
-        var valCurrent = indicatorActive
-        var objCurrent  = {}
-        objCurrent[keyCurrent] = valCurrent
-        this.setState(objCurrent)
-
-        var keyPrevious = 'indicator' + (this.state.currentStep - 1)
-        var valPrevious = indicatorFilled
-        var objPrevious  = {}
-        objPrevious[keyPrevious] = valPrevious
-        this.setState(objPrevious)
+        // var keyCurrent = 'indicator' + (this.state.currentStep)
+        // var valCurrent = indicatorActive
+        // var objCurrent  = {}
+        // objCurrent[keyCurrent] = valCurrent
+        // this.setState(objCurrent)
+        //
+        // var keyPrevious = 'indicator' + (this.state.currentStep - 1)
+        // var valPrevious = indicatorFilled
+        // var objPrevious  = {}
+        // objPrevious[keyPrevious] = valPrevious
+        // this.setState(objPrevious)
     }
 
     async _animationHandler() {
@@ -182,10 +168,27 @@ export default class AddRoute extends Component {
 
                 <View style={styles.navigation}>
                     <View style={styles.indicators}>
-                        <Animated.View style={this.state.indicator1}></Animated.View>
-                        <Animated.View style={this.state.indicator2}></Animated.View>
-                        <Animated.View style={this.state.indicator3}></Animated.View>
-                        <Animated.View style={this.state.indicator4}></Animated.View>
+
+                        <Animated.View style={[styles.indicator,
+                            {backgroundColor: this.state.indicator1.backgroundColor},
+                            {borderColor: this.state.indicator1.borderColor}]}>
+                        </Animated.View>
+
+                        <Animated.View style={[styles.indicator,
+                            {backgroundColor: this.state.indicator2.backgroundColor},
+                            {borderColor: this.state.indicator2.borderColor}]}>
+                        </Animated.View>
+
+                        <Animated.View style={[styles.indicator,
+                            {backgroundColor: this.state.indicator3.backgroundColor},
+                            {borderColor: this.state.indicator3.borderColor}]}>
+                        </Animated.View>
+
+                        <Animated.View style={[styles.indicator,
+                            {backgroundColor: this.state.indicator4.backgroundColor},
+                            {borderColor: this.state.indicator4.borderColor}]}>
+                        </Animated.View>
+
                     </View>
 
                     <TouchableHighlight
@@ -222,6 +225,15 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         flexDirection: 'row',
         marginBottom: 12
+    },
+    indicator: {
+        width: 8,
+        height: 8,
+        margin: 6,
+        borderRadius: 100,
+        borderWidth: 2,
+        backgroundColor: 'rgb(255,255,255)',
+        borderColor: 'rgb(204,204,204)',
     },
     button: {
         padding: 20,
