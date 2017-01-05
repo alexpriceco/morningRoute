5f87e6f6451b0a56696d6f9273cb25cc66bba59c
Alex Price <alex@alexprice.co>
2017-01-05 10:42:20 -0600

    Added previewElement, animated steps

--- components/AddRoute.js
+++ components/AddRoute.js
@@ -38,52 +38,58 @@ export default class AddRoute extends Component {
             indicator1: {},
             indicator2: {},
             indicator3: {},
-            indicator4: {}
+            indicator4: {},
+            inputsWrapper: { marginLeft: 0 }
         }
 
         const grey90 = 'rgb(35,35,35)'
         const blue50 = 'rgb(46,128,237)'
         const grey40 = 'rgb(204,204,204)'
-        const white  = 'rgb(255,255,255)'
+        const grey00 = 'rgb(255,255,255)'
 
         //indicator1
         this.state.indicator1.backgroundColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [white, blu, blu, blu]
+            outputRange: [grey00, blue50, blue50, blue50]
         }),
         this.state.indicator1.borderColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [blu, blu, blu, blu]
+            outputRange: [blue50, blue50, blue50, blue50]
         }),
 
         // indicator2
         this.state.indicator2.backgroundColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [grey, white, blu, blu]
+            outputRange: [grey40, grey00, blue50, blue50]
         }),
         this.state.indicator2.borderColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [grey, blu, blu, blu]
+            outputRange: [grey40, blue50, blue50, blue50]
         }),
 
         // indicator3
         this.state.indicator3.backgroundColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [grey, grey, white, blu]
+            outputRange: [grey40, grey40, grey00, blue50]
         }),
         this.state.indicator3.borderColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [grey, grey, blu, blu]
+            outputRange: [grey40, grey40, blue50, blue50]
         }),
 
         // indicator4
         this.state.indicator4.backgroundColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [grey, grey, grey, white]
+            outputRange: [grey40, grey40, grey40, grey00]
         }),
         this.state.indicator4.borderColor = this.state.currentStep.interpolate({
             inputRange:  [1, 2, 3, 4],
-            outputRange: [grey, grey, grey, blu]
+            outputRange: [grey40, grey40, grey40, blue50]
+        }),
+
+        this.state.inputsWrapper.marginLeft = this.state.currentStep.interpolate({
+            inputRange:  [1, 2, 3, 4],
+            outputRange: [0, (-Dimensions.get('window').width), (-2*(Dimensions.get('window').width)), (-3*(Dimensions.get('window').width))]
         })
     }
 
@@ -123,16 +129,24 @@ export default class AddRoute extends Component {
         let content
         if (this.props.isNew) {
             content = (
-                <View style={styles.inputsWrapper}>
-                    <Input placeholder='1234 Anydrive Road' setText={(text) => this.setState({inputsTo: text})}>Where are you going?</Input>
+                <Animated.View style={[styles.inputsWrapper, {marginLeft: this.state.inputsWrapper.marginLeft}]}>
+                    <Input style={styles.input} placeholder='1234 Anydrive Road' setText={(text) => this.setState({inputsTo: text})}>Where are you going?</Input>
 
-                    <Text style={styles.text}>When do you need to be there?</Text>
-                    <DatePickerIOS date={this.state.inputsWhen} mode='time' minuteInterval={15} onDateChange={(banana) => this.setState({inputsWhen: banana})} />
+                    <View style={styles.superInput}>
+                        <Text style={[styles.text, styles.input]}>When do you need to be there?</Text>
+                        <DatePickerIOS style={styles.input} date={this.state.inputsWhen} mode='time' minuteInterval={15} onDateChange={(banana) => this.setState({inputsWhen: banana})} />
+                    </View>
+
+                    <View style={styles.input}>
+                        <Text>UR MUM</Text>
+                        <View style={styles.previewElement}></View>
                     {/* <Preview></Preview> TODO: Build the preview component,
                         and pass in target address, and arrival time; will need
                         to hit the API. */}
-                    <Input placeholder='1234 Anydrive Road' setText={(text) => this.setState({inputsFrom: text})}>Where do you leave from?</Input>
-                </View>
+                    </View>
+
+                    <Input style={styles.input} placeholder='1234 Anydrive Road' setText={(text) => this.setState({inputsFrom: text})}>Where do you leave from?</Input>
+                </Animated.View>
             )
         } else {
             content = (
@@ -152,7 +166,7 @@ export default class AddRoute extends Component {
                     <Text style={styles.bold}>
                         Get updates on traffic conditions, and never be late to work again.
                     </Text>
-                    { content }
+                    <View style={styles.parentContainer}>{ content }</View>
                 </View>
 
                 <View style={styles.navigation}>
@@ -192,9 +206,10 @@ export default class AddRoute extends Component {
     }
 }
 
+
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: white,
+        backgroundColor: 'rgb(255,255,255)',
         flex: 1
     },
     content: {
@@ -207,7 +222,7 @@ const styles = StyleSheet.create({
         textAlign: 'left',
         marginVertical: 5,
         marginHorizontal: 30,
-        color: grey90,
+        color: 'rgb(35,35,35)',
         fontWeight: '600'
     },
     indicators: {
@@ -221,19 +236,19 @@ const styles = StyleSheet.create({
         margin: 6,
         borderRadius: 100,
         borderWidth: 2,
-        backgroundColor: white,
-        borderColor: grey40,
+        backgroundColor: 'rgb(255,255,255)',
+        borderColor: 'rgb(204,204,204)',
     },
     button: {
         padding: 20,
-        backgroundColor: blue50
+        backgroundColor: 'rgb(46,128,237)'
     },
     buttonText: {
         textAlign: 'center',
         fontSize: 20,
-        color: white
+        color: 'rgb(255,255,255)'
     },
-    inputsWrapper: {
+    parentContainer: {
         flexWrap: 'nowrap',
         width: Dimensions.get('window').width,
         flexDirection: 'row',
@@ -241,11 +256,25 @@ const styles = StyleSheet.create({
         alignSelf: 'stretch',
         backgroundColor: 'transparent'
     },
+    inputsWrapper: {
+        flexDirection: 'row',
+        width: (4*(Dimensions.get('window').width))
+    },
     text: {
         fontSize: 20,
         textAlign: 'left',
         marginVertical: 5,
         marginHorizontal: 30,
-        color: grey90
+        color: 'rgb(35,35,35)'
+    },
+    input: {
+        width: (Dimensions.get('window').width -60),
+        marginHorizontal: 30
+    },
+    previewElement: {
+        width: (Dimensions.get('window').width -60),
+        paddingVertical: 10,
+        backgroundColor: 'rgb(46,128,237)',
+        borderRadius: 8
     }
 })
